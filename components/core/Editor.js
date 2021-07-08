0d2ae65c0ad9be8bfe395b5985a01ed22c592d7f
saviomartin <saviomartin2007@gmail.com>
2021-07-08 14:00:08 +0530

    move to javascript

--- components/core/Editor.js
+++ components/core/Editor.js
@@ -4,32 +4,43 @@ import $ from "jquery";
 
 const Editor = () => {
   useEffect(() => {
-    var maxWidth = $("#outer").width();
-    var maxHeight = $("#outer").height();
+    const cover_image = document.getElementById("cover_image");
 
-    $(window).resize(function (evt) {
-      var $window = $(window);
-      var width = $window.width();
-      var height = $window.height();
-      var scale;
+    // get width and height of cover_image
+    const maxWidth = cover_image.offsetWidth;
+    const maxHeight = cover_image.offsetHeight;
+
+    window.onresize = () => {
+      // get width and height of window
+      const width = window.innerWidth;
+      const height = window.innerHeight;
+
+      let scale; // scale to be used
 
       // early exit
       if (width >= maxWidth && height >= maxHeight) {
-        $("#outer").css({ "-webkit-transform": "" });
-        $("#wrap").css({ width: "", height: "" });
+        cover_image.style.transform = "";
         return;
       }
 
-      scale = Math.min(width / maxWidth, height / maxHeight);
+      scale = Math.min(width / maxWidth, height / maxHeight); // trick to get scale
 
-      $("#outer").css({ "-webkit-transform": "scale(" + scale + ")" });
-      $("#wrap").css({ width: maxWidth * scale, height: maxHeight * scale });
-    });
+      cover_image.style.transform = `scale(${scale})`; // transform scale applied
+    };
   });
+
   return (
     <div className="h-full w-full lg:w-[65%] xl:w-[65%] relative bg-white flex items-center justify-center editor">
-      <div id="wrap" className="scale-[.85] lg:scale-[.65]">
-        <div id="outer"></div>
+      <div className="scale-[.85] lg:scale-[.65]">
+        <div id="cover_image">
+          <p className="text-5xl">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero nemo
+            laborum consectetur saepe, qui nisi possimus maiores id, est non
+            numquam! Id facilis vero quasi voluptatem ad blanditiis ullam error
+            architecto reprehenderit nisi? Ratione omnis quia qui. Laborum,
+            aspernatur sequi?
+          </p>
+        </div>
       </div>
     </div>
   );
