dd5f78d02efa57ee8569feeb9d9ff4a91b7d2cf4
saviomartin <saviomartin2007@gmail.com>
2021-07-23 12:02:57 +0530

    feat: custom css for clerk pages

--- pages/sign-in/[[...index]].js
+++ pages/sign-in/[[...index]].js
@@ -1,7 +1,9 @@
 import { SignIn } from "@clerk/clerk-react";
 
 const SignInPage = () => (
-  <SignIn path="/sign-in" routing="path" />
+  <div className="home h-screen w-full flex items-center justify-center">
+    <SignIn path="/sign-in" routing="path" />
+  </div>
 );
 
 export default SignInPage;
--- pages/sign-up/[[...index]].js
+++ pages/sign-up/[[...index]].js
@@ -1,7 +1,9 @@
 import { SignUp } from "@clerk/clerk-react";
 
 const SignUpPage = () => (
-  <SignUp path="/sign-up" routing="path" />
+  <div className="home h-screen w-full flex items-center justify-center">
+    <SignUp path="/sign-up" routing="path" />
+  </div>
 );
 
 export default SignUpPage;
